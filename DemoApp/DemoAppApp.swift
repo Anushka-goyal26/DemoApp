a4f208935e590c2fd839f85a4e2b0d8a71b3a102
anushka-goyal26 <anusshhka26@gmail.com>
2025-11-29 15:29:39 +0530

    Initial Commit

---
+++ DemoApp/DemoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoAppApp.swift
+//  DemoApp
+//
+//  Created by Student on 29/11/25.
+//
+
+import SwiftUI
+
+@main
+struct DemoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
